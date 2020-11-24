6b43a4c4f32ef31e856990bc904ecf7dc75e7f69
keipro00 <keipro00@outlook.jp>
2020-11-24 15:00:48 +0900

    新規登録、ログイン、ログアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,7 @@ class ItemsController < ApplicationController
   # indexファイルに遷移させる記述
   def index
   end
+  def new
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :comments
+  has_many :user_items
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+  
 end
