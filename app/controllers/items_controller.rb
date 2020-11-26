81a41eeff09c610c33517b0730de2466168b895e
keipro00 <keipro00@outlook.jp>
2020-11-26 14:02:07 +0900

    商品出品画面遷移、Activehash作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,24 @@
 class ItemsController < ApplicationController
   
+  before_action :authenticate_user!, only: [:new]
+  
+
   def new
-       if user_signed_in?
         @item = Item.new
-       else
-        redirect_to new_user_session_path
-  end
-end
+ end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+
+  def item_params
+    params.require(:item).permit(:category_id , :status_id, :delivery_fee_id, :prefexture_id, :delivery_day_id)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
- belongs_to :user
- has_many :comments
- has_one :user_item
+  belongs_to :user
+  has_many :comments
+  has_one :user_item
+  
+  # Activehashによるアソシエーション
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  #選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :delivery_fee, numericality: { other_than: 1 } 
+  validates :prefecture, numericality: { other_than: 0 } 
+  validates :delivery_day, numericality: { other_than: 1 } 
 
  with_options presence: true do
  validates :name
@@ -20,4 +35,5 @@ class Item < ApplicationRecord
 #  価格の条件設定はのちほど
 #  with_options presence: true, format: { with: /\A[\d]+\z/, /\A[300. + 9999999]+\z/ , message: '販売価格は、半角数値のみ' } do
 #  end
+
 end
